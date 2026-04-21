467edbc8c812255d80a0ed75746ca5458b04b04b
Vinca R Kurniawan <vincark19@gmail.com>
2026-04-21 23:50:20 +1000

    Add server.js and db.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cors = require('cors');
+const dotenv = require('dotenv');
+const pool = require('./src/config/db');
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Telehealth API is running!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
