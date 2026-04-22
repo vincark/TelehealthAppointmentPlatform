ec056ae81b4627b8f7730729bd2ad96965b88e8d
Vinca R Kurniawan <vincark19@gmail.com>
2026-04-22 21:50:12 +1000

    Add CORS restriction

--- server.js
+++ server.js
@@ -17,7 +17,11 @@ const loginLimiter = rateLimit({
 
 app.use('/api/auth/login', loginLimiter);
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 
 // Routes
