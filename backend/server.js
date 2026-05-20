41c3dae65fd3dc624d1741d43113e0e13e733896
Mooky Arthit <you@example.com>
2026-05-20 12:56:46 +1000

    remove console.log

--- backend/server.js
+++ backend/server.js
@@ -5,7 +5,6 @@ const pool = require('./src/config/db');
 const rateLimit = require('express-rate-limit');
 
 dotenv.config();
-console.log('FRONTEND_URL:', process.env.FRONTEND_URL);
 
 const app = express();
 
